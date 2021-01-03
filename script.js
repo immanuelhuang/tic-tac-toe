2ec95a049d20726aa4804e8c193b940790ab9f38
Immanuel Huang <immanuelgyhuang@gmail.com>
2021-01-02 21:26:06 -0500

    Updated game logic

--- script.js
+++ script.js
@@ -4,9 +4,21 @@ const gameBoard = (() => {
   const _ = 0;
 
   const boardState = [ _ , _ , _ , _ , _ , _ , _ , _ , _ ];
+  let playerTurn = _;
 
-  const clearBoard = () => {
+  const getBoardState = () => {boardState};
+  const getPlayerTurn = () => {playerTurn};
+
+  const printBoard = () => {
+    console.log("It's player " + playerTurn +  "'s turn.");
+    console.log(boardState[0] + " " + boardState[1] + " " + boardState[2] + "\n" +
+                boardState[3] + " " + boardState[4] + " " + boardState[5] + "\n" +
+                boardState[6] + " " + boardState[7] + " " + boardState[8])
+  }
+
+  const startGame = () => {
     for (let i = 0; i < boardState.length; i++) boardState[i] = _;
+    playerTurn = Math.floor(Math.random() * 2) === 0 ? X : O;
   };
 
   const checkWin = (space) => {
@@ -18,7 +30,7 @@ const gameBoard = (() => {
     let diagSum = boardState[0] + boardState[4] + boardState[8];
     let antiDiagSum = boardState[2] + boardState[4] + boardState[6];
 
-    console.table({rowSum, colSum, diagSum, antiDiagSum});
+    // console.table({rowSum, colSum, diagSum, antiDiagSum});
     if (rowSum === 3 || colSum === 3 || diagSum === 3 || antiDiagSum === 3) {
       return {
         winner: X,
@@ -27,9 +39,7 @@ const gameBoard = (() => {
         diag: diagSum === 3,
         antiDiag: antiDiagSum === 3,
       }
-    }
-    else if (rowSum === -3 || colSum === -3 || diagSum === -3 || antiDiagSum === -3) {
-      console.log("O WINS");
+    } else if (rowSum === -3 || colSum === -3 || diagSum === -3 || antiDiagSum === -3) {
       return {
         winner: O,
         row: [rowSum === -3, row],
@@ -37,19 +47,19 @@ const gameBoard = (() => {
         diag: diagSum === -3,
         antiDiag: antiDiagSum === -3,
       }
-    }
-    else {
+    } else {
+      if (!boardState.includes(_)) return {winner: _};
+      playerTurn = playerTurn === X ? O : X;
       return {winner: undefined};
     }
   }
 
-  const makeMove = (isX, space) => {
-    if (isX) boardState[space] = X;
+  const makeMove = (space) => {
+    if (playerTurn === X) boardState[space] = X;
     else boardState[space] = O;
-
+    printBoard();
     return checkWin(space);
   }
 
-  return {boardState, clearBoard, makeMove};
-
+  return {getBoardState, getPlayerTurn, startGame, makeMove, printBoard};
 })();
